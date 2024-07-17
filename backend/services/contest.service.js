b68bce6bc66a81e963c1dc6a4af97a3b23522947
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-07-17 23:52:12 +0530

    testing v1.07

--- backend/services/contest.service.js
+++ backend/services/contest.service.js
@@ -3,53 +3,48 @@ import sendEmailToAllUsers from './email.service.js';
 import { User } from '../model/user.model.js';
 
 const oneHourInMillis = 1 * 60 * 60 * 1000;
-const maxTime = 7 * 60 * 1000;
-const minTime = 3 * 60 * 1000; // minimum time difference to send the email
+const maxTime = 35 * 60 * 1000;
+const minTime = 30 * 60 * 1000; // minimum time difference to send the email
 
 const initialBiweeklyDate = new Date('2024-07-20T20:00:00+05:30'); // 20th July 2024, 8:00 PM IST
 
 
 const getChefContests = async () => {
-    // const response = await axios.get(`https://www.codechef.com/api/list/contests/all?sort_by=START&sorting_order=asc&offset=0&mode=all`);
-    // const contests = response.data.future_contests;
-    const contests = [
-        {
-            contest_code: 'START144',
-            contest_name: 'Starters 144 ',
-            contest_start_date: '17 Jul 2024  23:50:00',
-            contest_end_date: '24 Jul 2024  22:00:00',
-            contest_start_date_iso: '2024-07-17T23:45:00+05:30',
-            contest_end_date_iso: '2024-07-24T22:00:00+05:30',
-            contest_duration: '120',
-            distinct_users: 0
-        }
-    ]
+    const response = await axios.get(`https://www.codechef.com/api/list/contests/all?sort_by=START&sorting_order=asc&offset=0&mode=all`);
+    const contests = response.data.future_contests;
+    console.log('CodeChef contests:', contests);
+
+    // const contests = [
+    //     {
+    //         contest_code: 'START144',
+    //         contest_name: 'Starters 144 ',
+    //         contest_start_date: '17 Jul 2024  23:00:00',
+    //         contest_end_date: '24 Jul 2024  22:00:00',
+    //         contest_start_date_iso: '2024-07-24T20:00:00+05:30',
+    //         contest_end_date_iso: '2024-07-24T22:00:00+05:30',
+    //         contest_duration: '120',
+    //         distinct_users: 0
+    //     }
+    // ]
     // console.log('CodeChef contests:', contests);
-    // Convert contest start date from IST to SGT
-    const timeOfNextChefContestIST = new Date(contests[0].contest_start_date);
-    const timeDifferenceISTToSGT = 2.5 * 60 * 60 * 1000; // 2.5 hours in milliseconds
-    const timeOfNextChefContestSGT = new Date(timeOfNextChefContestIST.getTime() + timeDifferenceISTToSGT);
+    // const timeOfNextChefContest = new Date(contests[0].contest_start_date);
+    // console.log(timeOfNextChefContest.toLocaleString());
+    // const timeDifference = timeOfNextChefContest - Date.now();
+    // console.log(timeDifference);
+    // if (timeDifference > minTime && timeDifference < maxTime) {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
 
-    console.log('Contest start time in SGT:', timeOfNextChefContestSGT);
-    console.log('Current time in SGT:', new Date());
-
-    const timeDifference = timeOfNextChefContestSGT - Date.now();
-    console.log('Time difference:', timeDifference);
-
-    if (timeDifference > minTime && timeDifference < maxTime) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-
-    if (timeDifference >= minTime && timeDifference <= maxTime) {
-        // const message = `The ${contests[0].contest_name} contest on CodeChef starts within the next hour. Good luck!`;
-        const message = `testing sorry for bothering you!`;
-        const users = await User.find({ "platformProfiles.codechef.isId": true });
-        await sendEmailToAllUsers(message, users);
-    } else {
-        console.log('No CodeChef contests starting within the next hour.');
-    }
+    // if (timeDifference >= minTime && timeDifference <= maxTime) {
+    //     // const message = `The ${contests[0].contest_name} contest on CodeChef starts within the next hour. Good luck!`;
+    //     const message = `testing sorry for bothering you!`;
+    //     const users = await User.find({ "platformProfiles.codechef.isId": true });
+    //     await sendEmailToAllUsers(message, users);
+    // } else {
+    //     console.log('No CodeChef contests starting within the next hour.');
+    // }
 
     return contests;
 };
