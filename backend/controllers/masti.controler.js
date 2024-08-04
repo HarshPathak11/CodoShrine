dfe10e38c5e1d6d85d99c6b7e5ac718513fa9696
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-08-04 18:18:53 +0530

    Update masti.controler.js

--- backend/controllers/masti.controler.js
+++ backend/controllers/masti.controler.js
@@ -10,4 +10,16 @@ const getAllUsers = async (req, res) => {
     }
 };
 
-export { getAllUsers };
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ username: req.params.username });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully", user });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export { getAllUsers, deleteUser };
